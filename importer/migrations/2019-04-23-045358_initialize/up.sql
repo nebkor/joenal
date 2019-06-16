49234deb93fc611a97cdc7406a8923e0f5857ad2
Joe Ardent <code@ardent.nebcorp.com>
2019-06-16 15:09:44 -0700

    Add device ID to schemas.
    The medium is the message: the device on which you create something creates an indelible
    mark on whatever you're creating. This is meant to facilitate a primary key calculated by
    incorporating a device ID as well as the raw content.

--- importer/migrations/2019-04-23-045358_initialize/up.sql
+++ importer/migrations/2019-04-23-045358_initialize/up.sql
@@ -2,20 +2,22 @@
 CREATE TABLE jots (
        jot_id BLOB NOT NULL PRIMARY KEY,
        jot_creation_date TEXT,
-       jot_content TEXT
+       jot_content TEXT,
+       device_id BLOB
 );
 
 CREATE TABLE tags (
        tag_id BLOB NOT NULL PRIMARY KEY,
        tag_creation_date TEXT,
-       tag_text TEXT
+       tag_text TEXT,
+       device_id BLOB
 );
 
 CREATE TABLE tag_map (
        tag_id BLOB NOT NULL,
        jot_id BLOB NOT NULL,
        mapping_date TEXT,
-       mapping_id INTEGER NOT NULL PRIMARY KEY,
+       mapping_id BLOB NOT NULL PRIMARY KEY,
        FOREIGN KEY (tag_id) REFERENCES tags (tag_id) ON DELETE CASCADE ON UPDATE NO ACTION,
        FOREIGN KEY (jot_id) REFERENCES jots (jot_id) ON DELETE CASCADE ON UPDATE NO ACTION
 )
